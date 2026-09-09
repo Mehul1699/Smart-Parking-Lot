b8df26459ed526a667ea9e19524ffb0a6f269a82
Mehul1699 <mehul19356@gmail.com>
2026-09-10 00:18:50 +0530

    Initialize Spring Boot application

---
+++ src/main/java/com/airtribe/smart_parking_lot/SmartParkingLotApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.smart_parking_lot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingLotApplication.class, args);
+	}
+
+}
